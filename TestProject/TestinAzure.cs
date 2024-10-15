29c5ec8e0f30ac87e7bfd199e34b4d2110908b8f
vishal shukla <se.vishal98@gmail.com>
2024-10-15 15:58:35 +0530

    test in azure

---
+++ TestProject/TestinAzure.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class TestinAzure
+    {
+        public void MyTest(){
+            Console.WriteLine("Hello Test in Azure");
+        }
+    }
+}
